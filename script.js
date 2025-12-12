91a9fa6a52d2f5712a8b45a9b12eb1134ebb5be7
harshtahi <harsh@gmail.com>
2025-12-12 16:29:50 +0530

    bug fix

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-function beforesubmit(){
-    let outputdate=document.querySelector(".outputdate");
+let captchaChecked = false; //we made a flag variable for fuction
+
+
+
+function beforesubmit(event){
+    if(captchaChecked){
+         let outputdate=document.querySelector(".outputdate");
     let inputdate=document.querySelector(".inputdate");
     console.log("inputdate.value",inputdate);  //this date is in string form we have to convert it into locale(en_IN)
 
     let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
     outputdate.value=formattedDate;
+    }else{
+        alert("Please check the captcha");
+        event.preventDefault();
+    }
+    
 }
 
  function timestamp() { 
     var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+
+function captchasunccess(){
+    captchaChecked=true;
+}
